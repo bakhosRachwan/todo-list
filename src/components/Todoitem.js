42a6858d29daa9dc0e4ed548602a43c7fa831a31
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-03-04 23:37:56 +0200

    Feat: todo item

---
+++ src/components/Todoitem.js
@@ -0,0 +1,35 @@
+const TodoItem = ({task, date}) => {
+    return ( 
+          <tbody className="bg-white divide-y divide-gray-200">
+            <tr>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="flex items-center">
+                  <div className="ml-4">
+                    <div className="text-sm font-medium text-gray-900">
+                      {task}
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <div className="text-sm text-gray-900">{date}</div>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                  Active
+                </span>
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <input type="checkbox" />
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md dark:bg-gray-800 hover:bg-blue-500 dark:hover:bg-gray-700 focus:outline-none focus:bg-blue-500 dark:focus:bg-gray-700">
+                    Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+     );
+}
+ 
+export default TodoItem;
